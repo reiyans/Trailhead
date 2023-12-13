74326ccc7fe53bed0c498c09fa1cea867415aa1c
reiyans <reiyan1220@gmail.com>
2023-12-13 18:05:23 +0900

    Apex を使用したデータの操作を追加

---
+++ Lightning Web コンポーネントと Salesforce データ/Apex を使用したデータの操作/lwc/contactList/contactList.js
@@ -0,0 +1,16 @@
+import { LightningElement, wire } from 'lwc';
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';  
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+// 作成した Apex クラス 'ContactController' の Apex メソッド 'getContacts' をインポート
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+const COLUMNS = [
+    { label: 'First Name', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Last Name', fieldName: LASTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'Email' }
+];
+export default class ContactList extends LightningElement {
+    columns = COLUMNS;
+    @wire(getContacts)  // @wire を getContacts メソッドに使用し、データを取得
+    contacts;  // getContacts メソッドの結果を contacts プロパティに保存
+}
